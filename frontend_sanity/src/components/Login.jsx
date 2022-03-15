b007bae97290b73b4ba44bd671e57951d931a4e8
Dennys Solis <dennyssolis@gmail.com>
2022-03-15 16:16:31 -0400

    login

--- frontend_sanity/src/components/Login.jsx
+++ frontend_sanity/src/components/Login.jsx
@@ -6,11 +6,47 @@ import shareVideo from '../assets/share.mp4';
 import logo from '../assets/logowhite.png';
 
 const style = {
-  wrapper: ``,
-}
+  wrapper: `flex justify-start items-center flex-col h-screen`,
+  container: `relative w-full h-full`,
+  video:`w-full h-full object-cover`,
+  blackOverLay:`absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay`,
+  logoContainer:`p-5`,
+  googleLogin: `shadow-2xl`,
+  button: `bg-mainColor`,
+} 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <video 
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className={style.video}
+        />
+        <div className={style.blackOverLay}>
+          <div className={style.logoContainer}>
+            <img src={logo} width='130px' alt='logo' />
+          </div>
+          <div className={style.googleLogin}>
+            <GoogleLogin 
+              clientId=''
+              render={(renderProps) => (
+                <button 
+                  type='button'
+                  className={style.button}
+                >
+                  <FcGoogle className=
+                </button>
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
