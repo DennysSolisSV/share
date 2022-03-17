ded604e006b961d0b70593b86884a99c3ee6e3bb
Dennys Solis <dennyssolis@gmail.com>
2022-03-17 10:14:22 -0400

    sanity schema

--- backend_sanity/schemas/comment.js
+++ backend_sanity/schemas/comment.js
@@ -1,17 +1,19 @@
+
+  
 export default {
     name: 'comment',
     title: 'Comment',
     type: 'document',
     fields: [
-        {
-            name: 'postedBy',
-            title: 'PostedBy',
-            type: 'postedBy',
-        },
-        {
-            name: 'comment',
-            title: 'Comment',
-            type: 'string',
-        },        
-    ]
-}
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'comment',
+        title: 'Comment',
+        type: 'string',
+      },
+    ],
+  };
--- backend_sanity/schemas/save.js
+++ backend_sanity/schemas/save.js
@@ -3,15 +3,16 @@ export default {
     title: 'Save',
     type: 'document',
     fields: [
-        {
-            name: 'postedBy',
-            title: 'PostedBy',
-            type: 'postedBy',
-        },
-        {
-            name: 'userId',
-            title: 'UserId',
-            type: 'string',
-        },        
-    ]
-}
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'userId',
+        title: 'UserId',
+        type: 'string',
+      },
+    ],
+  };
+  
