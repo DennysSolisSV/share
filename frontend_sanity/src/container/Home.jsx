d31a915804be28b96858be20bebbf3fdbc77b866
Dennys Solis <dennyssolis@gmail.com>
2022-03-17 16:23:15 -0400

    index

--- frontend_sanity/src/container/Home.jsx
+++ frontend_sanity/src/container/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { link, Route, Routes } from 'react-router-dom';
+
+import { SideBar, UserProfile } from '../components';
+import Pins from './Pins';
+import { client } from '../client';
+import logo from '../assets/logo.png';
+
+const style = {
+  wrapper:`flex bg-gray-50 md:flex-row flex-col h-screen`,
+}
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={style.wrapper}>dfgdgdfg</div>
   )
 }
 
