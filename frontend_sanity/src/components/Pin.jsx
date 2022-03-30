dfb17ed7f6877708bd4662ecb79a897d138e4ecb
Dennys Solis <dennyssolis@gmail.com>
2022-03-30 16:21:45 -0400

    pin

--- frontend_sanity/src/components/Pin.jsx
+++ frontend_sanity/src/components/Pin.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const Pin = () => {
+import { MdDownloadForOfline } from 'react-icons/md';
+import { AiTwooneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRi} from 'react-icons/bs';
+
+import { client, urlFor } from '../client';
+
+const Pin = ({ pin: {postedBy, image, _id, destination }}) => {
+  const [ postHovered, setPostHovered ] = useState(false);
+  const [ savingPost, setSavingPost ] = useState(false);
+
+  const navigate = useNavigate();
   return (
-    <div>Pin</div>
+    <div className='m-2'>
+      <div 
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => Navigate(`/pin-detail/${_id}`)}
+        className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all'
+      >
+      </div>
+
+
+      <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
+    </div>
+    
   )
 }
 
