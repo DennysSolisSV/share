8f0acbfacf9db3f97bbe2d29b6df4f47bde61b80
Dennys Solis <dennyssolis@gmail.com>
2022-04-21 16:13:27 -0400

    Pin detail

--- frontend_sanity/src/components/PinDetail.jsx
+++ frontend_sanity/src/components/PinDetail.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+
+const PinDetail = ({ user }) => {
+const [pins, setPins] = useState(null);
+const [pinDetail, setPinDetails] = useState(null);
+const [comment, setComment] = useState('');
+const [addingComment, setAddingComment] = useState(false);
+
+const { pinId } = useParams();
+
+const fetchPinDetails = () => {
+  let query = pinDetailQuery(pinId);
+
+  if(query) {
+    client.fetch(query)
+      .then((data) => {
+        setPinDetails(data[0]);
+
+        if(data[0]){
+          query = pinDetailMorePinQuery(pinId);
+
+          client.fetch(query)
+            .then((res) => setPins(res));
+        }
+      })
+  }
+}
+
+useEffect(() => {
+  fetchPinDetails();
+}, [pinId])
+
+if(!pinDetail) return <Spinner message='Loading pin' />
+
   return (
-    <div>PinDetail</div>
+    <div className='flex xl-flex-row flex-col m-auto bg-white' style={{maxWidth:'1500px', borderRadius:'32px' }}>
+      <div className='flex justify-center items-center md:items-start flex-initial'>
+        <img 
+          src={pinDetail?.image && urlFor(pinDetail.image).url()}
+          className='rounded-t-3xl rounded-b-lg'
+          alt='user-post'
+        />
+      </div>
+      <div className='w-full p-5 flex-1 xl:min-w-620'>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-2 items-center'>
+          <a
+            href={`${pinDetail.image?.asset?.url}?dl=`}
+            download
+            onClick={(e) => e.stopPropagation()}
+            className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadows-md outline-none'
+          >
+            <MdDownloadForOffline />
+          </a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
